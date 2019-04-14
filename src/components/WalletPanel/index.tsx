9b02d267be265e394124b9d69bf3bcf93d2cc3e2
Hilmar Orth <hm.orth@aol.com>
2019-04-14 12:13:34 +0200

    edited WalletPanel to reflect InstantDX option

--- src/components/WalletPanel/index.tsx
+++ src/components/WalletPanel/index.tsx
@@ -20,7 +20,9 @@ const WalletPanel: React.SFC<WalletPanelProps> = ({ activeProvider, checkUserSta
     <AuctionAmountSummary />
     <AuctionPriceBar header="Price" />
     <AuctionWalletSummary />
+      <h2><strong>Regular DutchX Auction Process</strong></h2>
       <p>
+        <br></br>
         When submitting your order, you will be asked to sign transactions with {activeProvider || 'your Wallet provider'}.
         Explanations will be provided with each transaction.
         Upon final confirmation, your deposit will be added on your behalf to the next auction.
@@ -29,7 +31,19 @@ const WalletPanel: React.SFC<WalletPanelProps> = ({ activeProvider, checkUserSta
         Every auction takes approx. 6 hours.
       </p>
     <ButtonCTA onClick={checkUserStateAndSell}>
-      Submit Deposit <i className="icon icon-walletOK"></i>
+      Sell via regular auction process
+    </ButtonCTA>
+      <p>
+      <br></br>
+        OR
+      </p>
+      <h2><strong>Receive instant payouts</strong></h2>
+      <p>
+      <br></br>
+      Receive 80% of OMG tokens instanly and the remaining 20% after the auction has ended (approx 6 hours). Fee: 0.1% of final auction receivables. Click <a href="">here</a> for more info
+      </p>
+    <ButtonCTA  className="instant-dx" onClick={checkUserStateAndSell}>
+      Sell via InstantDX
     </ButtonCTA>
   </AuctionContainer>
 )
